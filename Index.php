7f461b32592305cc33a38714bb4325913f9050ab
ymebust <on5287ej@metrostate.edu>
2016-03-15 18:24:56 -0500

    Initial commit for Index.php

---
+++ Index.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Teufel Hunden
+ * Date: 3/15/2016
+ * Time: 5:19 PM
+ */
+?>
+<html xmlns:padding="http://www.w3.org/1999/xhtml">
+<head>
+	<style type="text/css">
+		h1 {
+			font-weight: bold;
+			font-size: 50px;
+			color: #0844A4;
+			text-align: center;
+			padding-top: 50px;
+		}
+		#info {
+			margin: auto;
+			width: 500px;
+			background-color: #D4E3FC;
+			text-align: center;
+			border: 1px solid black;
+			margin-bottom: 40px;
+		}
+		#people {
+			margin: auto;
+		}
+		p {
+			text-align: center;
+			font-size: 40px;
+		}
+		textarea {
+			background-color: #FFC3AE;
+			overflow-y: scroll;
+		}
+	</style>
+
+	<title>Event Details</title>
+</head>
+<body>
+<h1>Event Details</h1>
+<div id="info">
+<p>Saturday June 1st, 2016<br>Hilton Hotel<br>Minneapolis, Minnesota</p>
+</div>
+<div id="people">
+	<textarea rows="20" cols="40">Link to List of Names Attending</textarea>
+	<textarea rows="20" cols="40">Link to List of Donors</textarea>
+</div>
+</body>
+</html>
