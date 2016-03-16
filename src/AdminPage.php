93efc90769ac6f21ec5558b72a1db83f78f5034b
vikes123 <vikes011@gmail.com>
2016-03-15 22:05:40 -0500

    Added files via upload

---
+++ src/AdminPage.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<p style="text-align: center;"><strong><span style="color:#0000CD;"><span style="font-size:26px;"><span style="font-family:trebuchet ms,helvetica,sans-serif;">Administrator</span></span></span></strong></p>
+
+<p style="text-align: center;"><strong>Records</strong></p>
+
+<p style="text-align: center;"><input name="createRecordButton" type="button" value="Create Record" /></p>
+
+<p style="text-align: center;"><input name="editRecordButton" type="button" value="Edit Record" /></p>
+
+<p style="text-align: center;"><input name="deleteRecordButton" type="button" value="Delete Record" /></p>
+
+<p style="text-align: center;"><strong>Database</strong></p>
+
+<p style="text-align: center;"><strong><input name="uploadDatabaseButton" type="button" value="Upload Database" /></strong></p>
+
+<p style="text-align: center;"><strong><input name="downloadDatabaseButton" type="button" value="Download Database" /></strong></p>
+
+<p style="text-align: center;"><strong>Report</strong></p>
+
+<p style="text-align: center;"><strong><input name="reportButton" type="button" value="Generate Report" /></strong></p>
+
+<p style="text-align: center;">&nbsp;</p>
+
+<p style="margin-left: 160px;">&nbsp;</p>
+</body>
+</html>
