046ea0f9d210e64dcfd31bb33982c46c78b202b3
vikes123 <vikes011@gmail.com>
2016-03-15 22:00:17 -0500

    Added files via upload

---
+++ src/MainPage 2.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+	<title>HTML Editor - Full Version</title>
+</head>
+<body>
+<p style="text-align: center;"><span style="font-size:24px;"><span style="font-family:trebuchet ms,helvetica,sans-serif;"><strong><span style="color:#0000CD;">Village High School Reunion</span></strong></span></span></p>
+
+<p style="margin-left: 80px;"><input name="EnterEditButton" type="button" value="Enter or Edit your Info" />&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;<strong>Your Information</strong></p>
+
+<p style="margin-left: 80px;"><input name="databaseButton" type="button" value="Search Student/Teacher Database" />&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</p>
+
+<p style="margin-left: 80px;"><input name="adminButton" type="button" value="Administrator" /></p>
+
+<p style="margin-left: 80px;">&nbsp;</p>
+</body>
+</html>
