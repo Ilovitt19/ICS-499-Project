4ec8ec35594d91ea969cfc281bfd19b7112e39ff
ymebust <on5287ej@metrostate.edu>
2016-03-17 13:37:39 -0500

    index.php with test folder

---
+++ test/index.php
@@ -0,0 +1,15 @@
+<?php
+  include ('book_sc_fns.php');
+  // The shopping cart needs sessions, so start one
+  session_start();
+  do_html_header("Village High School Reunion","Event Info");
+
+  show_event_info();
+
+
+  // if logged in as admin, show add, delete, edit cat links
+  if(isset($_SESSION['admin_user'])) {
+    display_button("admin.php", "admin-menu", "Admin Menu");
+  }
+  do_html_footer();
+?>
