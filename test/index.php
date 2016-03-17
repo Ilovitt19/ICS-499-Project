6c7e4a0266bb250e12469c12c1b5f1f7b9fe92ef
ymebust <on5287ej@metrostate.edu>
2016-03-17 13:41:16 -0500

    Used the idea from bookorama. Output_fns is where all the html code is pulled from. This will help clean up the individual php pages.

--- test/index.php
+++ test/index.php
@@ -1,10 +1,8 @@
 <?php
-  include ('book_sc_fns.php');
+  include ('output_fns.php');
   // The shopping cart needs sessions, so start one
   session_start();
   do_html_header("Village High School Reunion","Event Info");
-
-  show_event_info();
 
 
   // if logged in as admin, show add, delete, edit cat links
