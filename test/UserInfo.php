9f04fff850c90a96ac8a934e6592f8fe96816fcf
Ian <ilovitt19@hotmail.com>
2016-03-21 16:50:14 -0500

    New branch

--- test/UserInfo.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include ('output_fns.php');
-
-do_html_header("My Profile","My Profile");
-
-do_student_info_form();
--- test/login.php
+++
@@ -1,8 +0,0 @@
-<?php
- require_once('output_fns.php');
- do_html_header("Login","Login");
-
- display_login_form();
-
- do_html_footer();
-?>
