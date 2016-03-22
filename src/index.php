1e05f469f8914e119cf10d7b121343a324a9f63a
ymebust <ymebust@gmail.com>
2016-03-22 18:29:59 -0500

    added login_check function to user_auth_fns to grant/block access to pages depending on login status

--- src/index.php
+++ src/index.php
@@ -1,9 +1,10 @@
 <?php
   include('output_fns.php');
-  // The shopping cart needs sessions, so start one
+  require_once('user_auth_fns.php');
+
   session_start();
   do_html_header("Village High School Reunion","Event Info");
 
-show_event_info();
+  show_event_info();
   do_html_footer();
 ?>
--- src/output_fns.php
+++ src/output_fns.php
@@ -13,10 +13,19 @@ function do_html_header($title = '',$section = '')
   <h1>Village High School Reunion</h1>
   <div id="topnavBar" class="topnavMenu">
   <ul class="navbar">
-  <li><a class="nav" title="Welcome" href="index.php">HOME</a></li>
-  <li><a class="nav" title="Profile" href="UserInfo.php">MY PROFILE</a></li>
-  <li><a class="nav" title="Find People" href="Search.php">FIND PEOPLE</a></li>
-  <?php
+    <li><a class="nav" title="Home" href="index.php">HOME</a></li>
+    <li><a class="nav" title="Profile" href="UserInfo.php">MY PROFILE</a></li>
+    <li><a class="nav" title="Find People" href="Search.php">FIND PEOPLE</a></li>
+    <?php
+    /*
+     * To hide navbar buttons if user is not logged in
+     *
+     *
+     *if (login_check() == 'true') {
+     *  echo "<li><a class='nav' title='Profile' href='UserInfo.php'>MY PROFILE</a></li>";
+     *  echo "<li><a class='nav' title='Find People' href='Search.php'>FIND PEOPLE</a></li>";
+     *}
+     */
   if (isset($_SESSION['admin_user'])) {
     echo "<li><a class = 'nav-right' title='Admin' href='admin.php'>ADMIN</a></li>";
   }
