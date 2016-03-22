a31acae0c1388d6f028ae5f6cdc9181cab612591
ymebust <ymebust@gmail.com>
2016-03-21 19:16:05 -0500

    CreateDatabase.php added user table for login info

--- src/CreateDatabase.php
+++ src/CreateDatabase.php
@@ -20,7 +20,7 @@ if ($mysql_connection->query($reunion) === TRUE) {
 
 $mysql_connection = new mysqli($db_hostname, $db_username, $db_password, $db_database);
 
-$students = "CREATE TABLE students(student_ID int NOT NULL AUTO_INCREMENT PRIMARY KEY, first_name varchar(15) NOT NULL,
+$students = "CREATE TABLE students(user_id int(6) PRIMARY KEY, first_name varchar(15) NOT NULL,
 last_name varchar(20) NOT NULL, nickname varchar(20), grad_year int(4) NOT NULL, father_name varchar(15),
 mother_name varchar(15),email varchar(40) NOT NULL, phone varchar(10), family_details varchar(100),
 work_experience varchar(200),awards varchar(200), street varchar(50), city varchar(20), state varchar(2),
@@ -35,7 +35,7 @@ if ($mysql_connection->query($students) === TRUE) {
 }
 
 
-$teachers = "CREATE TABLE teachers( teacher_ID int NOT NULL AUTO_INCREMENT PRIMARY KEY, first_name varchar(15) NOT NULL,
+$teachers = "CREATE TABLE teachers(user_id int(6) PRIMARY KEY, first_name varchar(15) NOT NULL,
  last_name varchar(20) NOT NULL,nickname varchar(20), start_year int(4) NOT NULL, end_year int(4) NOT NULL,
  father_name varchar(15), mother_name varchar(15),email varchar(40) NOT NULL, phone varchar(10),
  family_details varchar(100), work_experience varchar(200),awards varchar(200), street varchar(50),
@@ -47,5 +47,8 @@ if ($mysql_connection->query($teachers) === TRUE) {
 } else {
 	echo "Table not created: " . $mysql_connection->error;
 }
+
+$user = "CREATE TABLE user( user_id int NOT NULL AUTO_INCREMENT = 1000 FOREIGN KEY, username varchar(15) PRIMARY KEY,
+password varchar 40, admin varchar(3), user_type varchar(7))";
 
 $mysql_connection->close();
