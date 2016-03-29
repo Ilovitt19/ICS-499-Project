930f50d167b893efb950fd0242dec1907e4de1a0
ymebust <ymebust@gmail.com>
2016-03-29 18:42:19 -0500

    removed session_start()..

--- src/db_fns.php
+++ src/db_fns.php
@@ -2,7 +2,7 @@
 
 function db_connect() {
    //comment out metrostate linux sever when working on local machine
-   //$result = mysqli_connect('localbost', 'ics499sp160102', 'gjzzzcdm', 'ics499sp160102');
+   //$result = mysqli_connect('localhost', 'ics499sp160102', '299436', 'ics499sp160102');
    $result = mysqli_connect('localhost', 'root', '', 'reunion');
    if (!$result) {
       return false;
