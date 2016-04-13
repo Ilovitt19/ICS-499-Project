851b078b0be14a239843c7c95a6d75cddc460314
vikes011@gmail.com <dhak0002>
2016-04-13 16:57:32 -0500

    Removed "Login" title from Login page. There is already a graphic that displays the word "Login" so it was redundant.

--- src/login_page.php
+++ src/login_page.php
@@ -2,7 +2,7 @@
 require_once('reunion_fns.php');
 include('logged_in_user_class.php');
 if (isset($_SERVER['HTTP_REFERER'])) {
-  do_html_header("Login", "Login");
+  do_html_header("Login", " ");
 
   display_login_form();
 
