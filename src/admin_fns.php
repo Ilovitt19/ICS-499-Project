f47a12abf0c3fd6187598a7595d5532d8e3e62f0
vikes011@gmail.com <dhak0002>
2016-04-13 16:05:08 -0500

    cleaned up junk code

--- src/admin_fns.php
+++ src/admin_fns.php
@@ -57,50 +57,5 @@ function studentFunds () {
  */
 function teacherFunds () {
     $funds = "SELECT SUM(donation_field) FROM teachers";
-    echo "There are $" + $funds + "dollars raised from teachers ";
+    echo "There are $" + $funds + " dollars raised from teachers ";
 }
-
-//Store database array into an excel file
-//function arrayToExcel() {
-//    $array = db_result_to_array(db_connect());
-//
-//    header("Content-Disposition: attachment; filename=\"VillageHighSchoolReunionData.xls\"");
-//    header("Content-Type: application/vnd.ms-excel;");
-//    header("Pragma: no-cache");
-//    header("Expires: 0");
-//    $out = fopen("php://output", 'w');
-//    foreach ($array as $data)
-//    {
-//        fputcsv($out, $data,"\t");
-//    }
-//    fclose($out);
-//}
-
-//BELOW IS CODE TO EXPORT DATABASE DIRECTLY TO EXCEL FILE
-
-//function cleanData(&$str)
-//{
-//    $str = preg_replace("/\t/", "\\t", $str);
-//    $str = preg_replace("/\r?\n/", "\\n", $str);
-//    if(strstr($str, '"')) $str = '"' . str_replace('"', '""', $str) . '"';
-//}
-//
-//// filename for download
-//$filename = "VillageHighReunionDatabase_" . date('Ymd') . ".xls";
-//
-//header("Content-Disposition: attachment; filename=\"$filename\"");
-//header("Content-Type: application/vnd.ms-excel");
-//
-//$flag = false;
-//$result = pg_query("SELECT * FROM student") or die('Query failed!');
-//while(false !== ($row = pg_fetch_assoc($result))) {
-//    if(!$flag) {
-//        // display field/column names as first row
-//        echo implode("\t", array_keys($row)) . "\r\n";
-//        $flag = true;
-//    }
-//    array_walk($row, 'cleanData');
-//    echo implode("\t", array_values($row)) . "\r\n";
-//}
-//
-//exit;
