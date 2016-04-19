cb1f104d4187719e25feddc9ab1b9168dbf9d556
ymebust <ymebust@gmail.com>
2016-04-18 20:05:58 -0500

    removed old backup_database function

--- src/admin_fns.php
+++ src/admin_fns.php
@@ -121,77 +121,7 @@ function import_db_button () {
         <input class="fakebutton" type="submit" value="Submit" class="button" name="submit"><br>
     <?php
    }
-function export_database()
-{
-    $conn = db_connect();
-    //set time zone here
-    date_default_timezone_set("UTC");
 
-    $sql = "SELECT * FROM students";
-
-    $result = $conn->query($sql);
-    //output to file location change file path if needed. Default is src/sql_files/
-    $output_file = fopen("sql_files/reunion_db_" . date("dMY") . "_backup.sql", "w");
-
-    if ($result !== false) {
-
-        while ($row_array = $result->fetch_assoc()) {
-            $build_query = " INSERT INTO students (";
-            foreach ($row_array as $key => $value) {
-                $build_query .= $key . ", ";
-            }
-            $build_query = substr($build_query, 0, -2) . ") VALUES (";
-            foreach ($row_array as $key => $value) {
-                $build_query .= "'" . $value . "', ";
-            }
-            $build_query = substr($build_query, 0, -2) . ");\n";
-            fwrite($output_file, $build_query);
-
-        }
-    }
-    $sql = "SELECT * FROM teachers";
-
-    $result = $conn->query($sql);
-
-    if ($result !== false) {
-
-        while ($row_array = $result->fetch_assoc()) {
-            $build_query = " INSERT INTO teachers (";
-            foreach ($row_array as $key => $value) {
-                $build_query .= $key . ", ";
-            }
-            $build_query = substr($build_query, 0, -2) . ") VALUES (";
-            foreach ($row_array as $key => $value) {
-                $build_query .= "'" . $value . "', ";
-            }
-            $build_query = substr($build_query, 0, -2) . ");\n";
-            fwrite($output_file, $build_query);
-
-        }
-    }
-    $sql = "SELECT * FROM user";
-
-    $result = $conn->query($sql);
-
-    if ($result !== false) {
-
-
-        while ($row_array = $result->fetch_assoc()) {
-            $build_query = " INSERT INTO user (";
-            foreach ($row_array as $key => $value) {
-                $build_query .= $key . ", ";
-            }
-            $build_query = substr($build_query, 0, -2) . ") VALUES (";
-            foreach ($row_array as $key => $value) {
-                $build_query .= "'" . $value . "', ";
-            }
-            $build_query = substr($build_query, 0, -2) . ");\n";
-            fwrite($output_file, $build_query);
-
-        }
-    }
-    $conn->close();
-}
 
 function do_stats_output() {
     ?>
